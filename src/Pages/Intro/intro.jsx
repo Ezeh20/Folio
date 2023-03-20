0831469e234228207646c41b43881548cc0ea890
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2023-03-20 02:14:34 +0100

    style:loading animation

---
+++ src/Pages/Intro/intro.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './intro.module.scss'
+
+const Intro = () => {
+    return (
+        <div>
+            <svg className={styles.svg} id="eyNd7PMvkZf1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 40 40" shape-rendering="geometricPrecision" text-rendering="geometricPrecision"><text dx="0" dy="0" font-family="&quot;eyNd7PMvkZf1:::Montserrat&quot;" font-size="15" font-weight="400" transform="matrix(1.232748 0 0 1.290159 9.401886 28.316734)" fill="#61f8d5" stroke-width="0"><tspan y="0" font-family="&quot;eyNd7PMvkZf1:::Montserrat&quot;" font-size="15" font-weight="400" font-style="normal" fill="#61f8d5" stroke-width="0">
+
+                C
+            </tspan></text><text className={styles.C} dx="0" dy="0" font-family="&quot;eyNd7PMvkZf1:::Montserrat&quot;" font-size="15" font-weight="400" transform="matrix(1.48477 0 0 1.777464 19.518888 39.372825)" fill="#61f8d5" stroke-width="0"><tspan y="0" font-family="&quot;eyNd7PMvkZf1:::Montserrat&quot;" font-size="15" font-weight="400" font-style="normal" fill="#61f8d5" stroke-width="0">
+
+                J
+
+            </tspan></text><path className={styles.path} d="M0,0h46.236128v27.741677h-46.236128L0,0Z" transform="matrix(.614483 0 0 0.821315 5.794347 10.714867)" fill="none" stroke="#61f8d5" />
+
+            </svg>
+
+
+        </div>
+    )
+}
+
+export default Intro
