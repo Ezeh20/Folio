01be24440cf428a9ff9aeed69dc2f5c94919478d
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2023-03-23 22:08:17 +0100

    style:nav animation

--- src/Components/Navigation/data.js
+++ src/Components/Navigation/data.js
@@ -1,25 +1,25 @@
 const menu = [
     {
         id: 0,
-        number: `01. `,
+        number: `01 . `,
         text: 'About',
         to: 'about',
     },
     {
         id: 1,
-        number: `02. `,
+        number: `02 . `,
         text: 'Experience',
         to: 'experience',
     },
     {
         id: 2,
-        number: `03. `,
+        number: `03 . `,
         text: 'Work',
         to: 'work'
     },
     {
         id: 3,
-        number: `04. `,
+        number: `04 . `,
         text: 'Contact',
         to: 'contact',
     }
--- src/Components/Navigation/index.jsx
+++ src/Components/Navigation/index.jsx
@@ -3,42 +3,75 @@ import { Link } from 'react-router-dom'
 import Container from '../Container/container'
 import menu from './data'
 import styles from './navigation.module.scss'
-import {  motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import Button from '../Button/button'
-
 
 const Navigation = () => {
     const [toggle, setToggle] = useState(false)
-    const variants = {
-        open: {opacity: 1, x: 0 },
-        closed: { opacity: 0, x: "100%" },
+    const listsVariant = {
+        hidden: {
+            x: "100%",
+            opacity: 0,
+
+        },
+        visible: {
+            x: 0,
+            opacity: 1,
+            transition: {
+               
+                when: "beforeChildren"
+            }
+        }
     }
+    const listVariant = {
+        hidden: {
+            opacity: 0,
+            y:-100
+
+        },
+        visible: {
+            opacity: 1,
+            y:0,
+            transition: {
+                delay:1,
+                staggerChildren: 5,
+            }
+        }
+    }
+
     return (
         <motion.header className={styles.header}>
             <Container>
                 <nav className={styles.nav}>
-                    <div className={styles.logoContainer}>
-                        <img src="/public/logo.svg" alt="logo" className={styles.logo} />
-                    </div>
-                    <div className={styles.menuShow}>
+                    <Link to='/' className={styles.logoContainer}>
+                        <motion.img
+                            initial={{ scale: 0, opacity: 0 }}
+                            animate={{ scale: 1, opacity: 1 }}
+                            transition={{ duration: .5, type: "spring" }}
+                            src="/public/logo.svg" alt="logo" className={styles.logo} />
+                    </Link>
+                    <div
+                        className={styles.menuShow}>
                         <div className={toggle && styles.backdropalt} />
-                            <motion.ul
-                                animate={toggle ? 'open' : 'closed'}
-                                transition={{ duration: .5 }}
-                                variants={variants}
-                                className={!toggle ? styles.menus : `${`${styles.menus} ${styles.menusOpen}`}`}>
-                                {
-                                    menu.map((itm) => {
-                                        const { id, number, text, to } = itm
-                                        return (
-                                            <li className={styles.list} key={id}>
-                                                <span>{number}</span>  <Link to={to} className={styles.navi}>{text}</Link>
-                                            </li>
-                                        )
-                                    })
-                                }
-                                <Button>Resume</Button>
-                            </motion.ul>
+                        <motion.ul
+                            variants={listsVariant}
+                            animate="visible"
+                            initial="hidden"
+                            className={!toggle ? styles.menus : `${`${styles.menus} ${styles.menusOpen}`}`}>
+                            {
+                                menu.map((itm) => {
+                                    const { id, number, text, to } = itm
+                                    return (
+                                        <motion.li
+                                            variants={listVariant}
+                                            className={styles.list} key={id}>
+                                            <span>{number}</span>  <Link to={to} className={styles.navi}>{text}</Link>
+                                        </motion.li>
+                                    )
+                                })
+                            }
+                            <Button >Resume</Button>
+                        </motion.ul>
                     </div>
                     <div className={styles.buger} onClick={() => setToggle(!toggle)}>
                         <div className={!toggle ? styles.line1 : `${`${styles.line1} ${styles.line1alt}`}`} />
