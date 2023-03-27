5f6d838668eda1984d54d1a0ebe182a65122f119
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2023-03-27 13:11:11 +0100

    style: image

--- src/Pages/Home/about-section/image/image.jsx
+++ src/Pages/Home/about-section/image/image.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from './image.module.scss'
 
 const Images = () => {
-  return (
-    <div>Images</div>
-  )
+    return (
+        <div className={styles.person}>
+            <div className={styles.personContainer}>
+                <div className={styles.filter} />
+                <div className={styles.halo} />
+            </div>
+        </div>
+    )
 }
 
 export default Images
