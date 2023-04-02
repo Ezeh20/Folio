579910f3d0cc59407e9f78fa196d9ddc2af94acc
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2023-04-02 14:41:59 +0100

    UI: work

--- src/Pages/Home/projects-section/data.jsx
+++ src/Pages/Home/projects-section/data.jsx
@@ -8,9 +8,45 @@ const works = [
         projectDescription: `This is a CRUD application which makes use of firebase as a backend. Users must login or signup to cre
         ate a task. When ticked as 'completed', that task will be saved in a completed tasks collection which shows the task, taskId
         start time and end time.There is also an xp system where each completed task grants 10 points which then unlocks awards if the needed 
-        points are enough`,
+        points are enough.`,
         tools: ['React', 'Sass', 'Framer', 'Firebase'],
+        github: <FiGithub />,
+        link: <FiLink />
+    },
+    {
+        id: 2,
+        img: 'https://i.ibb.co/tcx5LGm/middle-man.png',
+        projectName: 'Middle man',
+        projectDescription: `Middle man is a loan agreement generator that generates an agreement between two parites. The user needs to 
+        fill in the required information and must select between two variations [mercy , wickedness] then an agreement will be generated based
+        on the filled form. I learnt how to utilize localStorage and also how to make use of user's data collected through a form.`,
+        tools: ['React', 'Sass'],
+        github: <FiGithub />,
+        link: <FiLink />
+    },
+    {
+        id: 3,
+        img: 'https://i.ibb.co/0GpvY8p/udemy.png',
+        projectName: 'Udemy attempt',
+        projectDescription: `This is my attempt on the udemy home page. I used alot of html 3861 lines to be precise which are mostly repeated.
+        I hard coded 7 categories which has 8 items each and the 'student's are viewing section' which has 15 items when i would have just created the elements
+        by manipulating the dom by feeding on the needed object data. Using react later on made this very clear to me.`,
+        tools: ['Html', 'Css', 'JavaScript'],
+        github: <FiGithub />,
+        link: <FiLink />
+    },
+    {
+        id: 4,
+        img: 'https://i.ibb.co/c6pCN8K/e-mart.png',
+        projectName: 'E-mart',
+        projectDescription: `This is a CRUD application which makes use of firebase as a backend. Users must login or signup to cre
+        ate a task. When ticked as 'completed', that task will be saved in a completed tasks collection which shows the task, taskId
+        start time and end time.There is also an xp system where each completed task grants 10 points which then unlocks awards if the needed 
+        points are enough.`,
+        tools: ['React', 'Sass', 'Firebase'],
         github: <FiGithub />,
         link: <FiLink />
     }
 ]
+
+export default works
--- src/Pages/Home/projects-section/projects.jsx
+++ src/Pages/Home/projects-section/projects.jsx
@@ -3,6 +3,7 @@ import Container from '../../../Components/Container/container'
 import ContentContainer from '../../../Components/ContainerContents/container'
 import Line from '../../../Components/Line/line'
 import styles from './projects.module.scss'
+import works from './data'
 
 const Projects = () => {
   return (
@@ -13,24 +14,36 @@ const Projects = () => {
             <p className={`heading head`}><span className='spans'>03.</span>Work</p>
             <Line />
           </div>
-          <section className={styles.section} id='work'>
-            <div className={styles.prjScreenshot}>
-              <img src="/public/img/todo profile.png" alt="img" className={styles.prj} />
-              <div className={styles.overlay} />
-            </div>
-            <div className={styles.prjDetails}>
-              <p className={styles.prjName}>Project 1</p>
-              <p className={styles.projectDetails}>A project about nyash A project about nyash A project about nyash A project about nyash A project about nyash A project about nyash A project about nyash A project about nyash A project about nyash A project about nyash  A project about nyash A project about nyash A project about nyash A project about nyash A project about nyash A project about nyash  A project about nyash A project about nyash A project about nyash  </p>
-              <div className={styles.toolsGrp}>
-                <div className={styles.tools}>
-                  <p>Js</p>
-                </div>
-                <div className={styles.links}>
-                  <p>github</p>
-                </div>
-              </div>
-            </div>
-          </section>
+          {
+            works.map(itm => {
+              const { id, img, projectName, projectDescription, tools, github, link } = itm
+              return (
+                <section className={styles.section} id='work'>
+                  <div className={id % 2 !== 0 ? styles.prjScreenshot : `${`${styles.prjScreenshotAlt} ${styles.prjScreenshot}`}`}>
+                    <img src={img} alt="img" className={styles.prj} />
+                    <div className={styles.overlay} />
+                  </div>
+                  <div className={id % 2 !== 0 ? styles.prjDetails : `${`${styles.prjDetailsAlt} ${styles.prjDetails}`}`}>
+                    <p className={styles.prjName}>{projectName}</p>
+                    <p className={styles.projectDetails}>{projectDescription}</p>
+                    <div className={styles.toolsGrp}>
+                      <div className={styles.tools}>
+                        {tools.map(tool => {
+                          return (
+                            <div>{tool}</div>
+                          )
+                        })}
+                      </div>
+                      <div className={styles.links}>
+                        <p className={styles.link}>{github}</p>
+                        <p className={styles.link}>{link}</p>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              )
+            })
+          }
         </div>
       </ContentContainer>
     </Container>
