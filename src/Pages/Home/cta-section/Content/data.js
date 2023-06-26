e743972b06023971a93714b63168a189aada88b0
Ezeh20 <ranter1570@gmail.com>
2023-06-26 18:50:44 +0100

    edit: edited the write up

--- src/Pages/Home/cta-section/Content/data.js
+++ src/Pages/Home/cta-section/Content/data.js
@@ -2,8 +2,9 @@ const content = {
     greeting: 'hello, my name is',
     name: 'Chijioke Ezeh',
     role: `A Frontend Web Developer`,
-    details: `As a frontend web developer, my passion lies in creating visually pleasing and user-friendly web apps.
-     My goal is to achieve pixel-perfect designs while prioritizing the user's experience. Thank you`,
+    details: `As a frontend web developer, i create visually pleasing and user-friendly web apps.
+     My goal is to achieve pixel-perfect designs and 
+     implementation to problems while prioritizing the user's experience. Thank you.`,
     cta: 'Contact me',
 }
 
